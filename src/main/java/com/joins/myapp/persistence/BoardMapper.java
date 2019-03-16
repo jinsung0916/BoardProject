eafe09a720b4236d394be136f7ab492490feea15
안진성 <jinsung0916@naver.com>
2019-03-17 02:32:42 +0900

    기간검색 관련 mapper.xml 설정 구현중...

--- src/main/java/com/joins/myapp/persistence/BoardMapper.java
+++ src/main/java/com/joins/myapp/persistence/BoardMapper.java
@@ -1,5 +1,6 @@
 package com.joins.myapp.persistence;
 
+import java.util.Date;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Param;
@@ -12,7 +13,10 @@ public interface BoardMapper {
     BoardDTO findByNo(long id);
 
     List<BoardDTO> findPagenated(@Param("startIndex") int startIndex, @Param("itemsPerPage") int itemsPerPage);
-
+    
+    List<BoardDTO> findByDateAndTitle(@Param("startDate") Date startDate, @Param("endDate") Date endDate, 
+	    @Param("title") String title);
+    
     int insert(BoardDTO board);
 
     int update(BoardDTO board);
