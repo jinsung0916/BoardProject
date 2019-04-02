3c0af273f2efc244b0d06f0178753d5c1e6b6130
unknown <jinsung0916@naver.com>
2019-04-02 16:45:32 +0900

    ...

--- src/main/java/com/joins/myapp/controller/SecurityController.java
+++ src/main/java/com/joins/myapp/controller/SecurityController.java
@@ -6,10 +6,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import lombok.extern.slf4j.Slf4j;
 
+/*
+ * Spring Security 관련 URL 처리를 위한 컨트롤러	
+ */
 @Controller
 @Slf4j
 public class SecurityController {
 
+    /**
+     * 1. 개요: GET /customLogin
+     * 2. 처리내용: 로그인 페이지를 반환한다.
+     * 3. 입력 Data:
+     * 4. 출력 Data: 
+     */
     @GetMapping("/customLogin")
     public String loginInput(String error, String logout, Model model) {
 	log.info("error: {}", error);
@@ -25,6 +34,12 @@ public class SecurityController {
 	return "auth/login";
     }
 
+    /**
+     * 1. 개요: GET /logout
+     * 2. 처리내용: 로그아웃 페이지를 반환한다.
+     * 3. 입력 Data:
+     * 4. 출력 Data: 
+     */
     @GetMapping("/logout")
     public String logout() {
 	return "auth/logout";
